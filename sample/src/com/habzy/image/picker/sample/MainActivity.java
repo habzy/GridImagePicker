5be79c425164e4ccfd2869ed64aabafc8ec97350
Habzy Huang <habzyhs@gmail.com>
2014-10-15 18:31:05 +0800

    Remove unused import

--- sample/src/com/habzy/image/picker/sample/MainActivity.java
+++ sample/src/com/habzy/image/picker/sample/MainActivity.java
@@ -15,7 +15,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
